da5324774bc9edd87954bd0e5efe2c15080e1d6a
stealyglint <stealyglint@gmail.com>
2017-11-18 23:01:53 +0000

    Added RTC functions
    Connected 3231 RTC module and used adafruit RTClib.

--- arduino-chc.ino
+++ arduino-chc.ino
@@ -1,13 +1,31 @@
+#include <Wire.h>
+#include "RTClib.h"
+
 #define RL1 10  // relay 1 will be on PIN 10
 #define LED1 11
 #define SW1 2
 #define SW2 3
 
+// Date and time functions using a DS3231 RTC connected via I2C and Wire lib
+RTC_DS3231 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
 boolean demand = HIGH;
 boolean chstate = LOW;
 
+
+
 void setup() {
   // put your setup code here, to run once:
+
+  Serial.begin(9600);
+
+  delay(3000); // wait for console opening
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
 // initialize digital pins RL1 LED1 SW1 SW2 and set RL1 to off.
   pinMode(RL1, OUTPUT);
   pinMode(LED1, OUTPUT);
@@ -16,8 +34,9 @@ void setup() {
   digitalWrite(RL1, demand);
 }
 
-void loop() {
+void loop(){
   // Main loop for controller:
+ 
   if (digitalRead(SW1) == HIGH)
   {
     if (demand == LOW){
@@ -34,8 +53,47 @@ void loop() {
 
    if (chstate == HIGH){
     digitalWrite(RL1, demand);
-    
+    showDate(demand);
     delay(3000);
     chstate = LOW;
+    
    }
+
  }
+
+void showDate(boolean state) {
+  DateTime now = rtc.now();
+
+  if (state==LOW){
+      
+     Serial.print("Heating Switched ON!  " );
+    }
+  else if ( state==HIGH) {
+    Serial.print("Heating Switched OFF!  " );
+  }
+  
+    
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    print2digits(now.hour());
+    Serial.print(':');
+   print2digits(now.minute());
+    Serial.print(':');
+   print2digits(now.second());
+   
+    Serial.println();
+
+}
+
+ void print2digits(int number) {
+  if (number < 10) {
+    Serial.print("0"); // print a 0 before if the number is < than 10
+  }
+  Serial.print(number);
+}
